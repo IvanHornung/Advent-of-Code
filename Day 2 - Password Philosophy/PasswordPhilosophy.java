b5dfe13d350f2a1e10b87760937b183d2a649ed2
Ivan Hornung <ivanhor2003@gmail.com>
2020-12-02 14:09:51 -0800

    Reorganized repo + improved problem 2's efficiency

--- Day 2 - Password Philosophy/PasswordPhilosophy.java
+++ Day 2 - Password Philosophy/PasswordPhilosophy.java
@@ -1,10 +1,9 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class PasswordPhilosophy {
-    static int validPasswordCount = 0;
+    static int validPasswordsPart1 = 0, validPasswordsPart2 = 0;
 
     public static void main(String[] args) {
         //PART 1
@@ -16,36 +15,25 @@ public class PasswordPhilosophy {
                 int upperBound = Integer.parseInt(line.substring(line.indexOf("-") + 1, line.indexOf(" ")));
                 char letter = line.substring(line.indexOf(" ") + 1).charAt(0);
                 String meat = line.substring(line.indexOf(letter) + 3);
+                //PART 1
                 int letterCount = 0;
                 for (int i = 0; i < meat.length(); i++) {
                     if (meat.charAt(i) == letter)
                         letterCount++;
                 }
                 if(letterCount >= lowerBound && letterCount <= upperBound)
-                    validPasswordCount++;
-            }
-        } catch (FileNotFoundException exception) {}
-        System.out.println(validPasswordCount);
+                    validPasswordsPart1++;
 
-        //PART 2
-        validPasswordCount = 0;
-        try {
-            Scanner input = new Scanner(new File("input.txt"));
-            while (input.hasNextLine()) {
-                String line = input.nextLine();
-                int lowerPosition = Integer.parseInt(line.substring(0, line.indexOf("-")));
-                int upperPosition = Integer.parseInt(line.substring(line.indexOf("-") + 1, line.indexOf(" ")));
-                char letter = line.substring(line.indexOf(" ") + 1).charAt(0);
-                String meat = line.substring(line.indexOf(letter) + 3);
+                //PART 2
                 boolean first = false, second = false;
-                if(meat.charAt(lowerPosition-1) == letter)
+                if(meat.charAt(lowerBound-1) == letter)
                     first = true;
-                if(meat.charAt(upperPosition-1) == letter)
+                if(meat.charAt(upperBound-1) == letter)
                     second = true;
                 if((first && !second) || (second && !first))
-                    validPasswordCount++;
+                    validPasswordsPart2++;
             }
         } catch (FileNotFoundException exception) {}
-        System.out.println(validPasswordCount);
+        System.out.println(validPasswordsPart1 + "\n" + validPasswordsPart2);
     }
 }
